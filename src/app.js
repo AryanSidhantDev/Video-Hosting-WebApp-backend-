d8b5decf2335653f613b7f8e92fe9fe818e6126d
Aryan <aryansidhantdev@gmail.com>
2024-06-15 00:07:27 +0530

    utils code

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import expess from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+
+const app=express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials:true 
+}))
+
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(ezpress.static("public"))
+app.use(cookieParser())
+
+
+export { app }
